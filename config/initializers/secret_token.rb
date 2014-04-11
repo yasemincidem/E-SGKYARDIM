33da8f8c03ef7e835908e6be53d9ce9f25f67183
skylightcoder <skylightcoder@gmail.com>
2014-04-11 11:06:28 +0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ESgk::Application.config.secret_key_base = 'f519ffe662842e17aa444478dd7ab293e2dd2e3625e2045e04cd12e87f55949eced9655508fe1b50bf9afbd7c370581149bf41ccfa86107659e6ef7d96376638'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ESgk::Application.config.session_store :cookie_store, key: '_E-SGK_session'
